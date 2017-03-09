18e328fb5c8becb71ec40b4ff25ed24033a3612c
ayoa77 <ayodeleamadi@gmail.com>
2017-03-09 16:23:34 +0800

    basic carrierwave functionality added

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# Used for resizing images
+gem "mini_magick"
+# Used carrierwave to upload photos
+gem 'carrierwave'
 #user password gem
 gem 'clearance'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+require 'carrierwave/orm/activerecord'
+
+# CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       :provider               => 'AWS',            # required
+#       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],     # required
+#       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],  # required
+#       :region                 => 'ap-southeast-1'
+#     }
+#     config.fog_use_ssl_for_aws = false
+#     config.fog_directory       = ENV['S3_BUCKET']
+#   end         # required
